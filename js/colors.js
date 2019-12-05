b46ced4a2df7d73d2281222933962224ba527b79
xtrp <36894883+xtrp@users.noreply.github.com>
2019-12-05 18:15:59 +0000

    add modal

--- js/colors.js
+++ js/colors.js
@@ -3,36 +3,66 @@ const colors = [
         name: "Shard Blue",
         description: "The Shard, Central London",
         imagePath: "../img/shard.jpg",
-        hex: "#2188ff"
+        hex: "#2188ff",
+        modalHTML: `
+        <p><strong>Location:</strong><span>The Shard, Central London, SW5 FT1</span></p>
+        <p><strong>Represents:</strong><span>Intellect, security, trust, uniqueness</span></p>
+        <p><strong>Backstory:</strong><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></p>
+        `
     },
     {
         name: "Shard Blue",
         description: "The Shard, Central London",
         imagePath: "../img/shard.jpg",
-        hex: "#e74c3c"
+        hex: "#e74c3c",
+        modalHTML: `
+        <p><strong>Location:</strong><span>The Shard, Central London, SW5 FT1</span></p>
+        <p><strong>Represents:</strong><span>Intellect, security, trust, uniqueness</span></p>
+        <p><strong>Backstory:</strong><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></p>
+        `
     },
     {
         name: "Shard Blue",
         description: "The Shard, Central London",
         imagePath: "../img/shard.jpg",
-        hex: "#2188ff"
+        hex: "#2188ff",
+        modalHTML: `
+        <p><strong>Location:</strong><span>The Shard, Central London, SW5 FT1</span></p>
+        <p><strong>Represents:</strong><span>Intellect, security, trust, uniqueness</span></p>
+        <p><strong>Backstory:</strong><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></p>
+        `
     },
     {
         name: "Shard Blue",
         description: "The Shard, Central London",
         imagePath: "../img/shard.jpg",
-        hex: "#2188ff"
+        hex: "#2188ff",
+        modalHTML: `
+        <p><strong>Location:</strong><span>The Shard, Central London, SW5 FT1</span></p>
+        <p><strong>Represents:</strong><span>Intellect, security, trust, uniqueness</span></p>
+        <p><strong>Backstory:</strong><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></p>
+        `
     },
     {
         name: "Shard Blue",
         description: "The Shard, Central London",
         imagePath: "../img/shard.jpg",
-        hex: "#2188ff"
+        hex: "#2188ff",
+        modalHTML: `
+        <p><strong>Location:</strong><span>The Shard, Central London, SW5 FT1</span></p>
+        <p><strong>Represents:</strong><span>Intellect, security, trust, uniqueness</span></p>
+        <p><strong>Backstory:</strong><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></p>
+        `
     },
     {
         name: "Shard Blue",
         description: "The Shard, Central London",
         imagePath: "../img/shard.jpg",
-        hex: "#2188ff"
+        hex: "#2188ff",
+        modalHTML: `
+        <p><strong>Location:</strong><span>The Shard, Central London, SW5 FT1</span></p>
+        <p><strong>Represents:</strong><span>Intellect, security, trust, uniqueness</span></p>
+        <p><strong>Backstory:</strong><span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></p>
+        `
     }
 ];
