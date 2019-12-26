d0f1ee5c3d624f035db75a179485c3af7bf22cb7
xtrp <36894883+xtrp@users.noreply.github.com>
2019-12-26 20:01:35 +0000

    start hhome

--- index.php
+++ index.php
@@ -12,32 +12,34 @@ include "assets/php/vendors/Parsedown.php";
 ?>
 
 <body>
-    <div class="center-area">
-        <nav class="center-area__topbar">
-            <a class="center-area__topbar__logo">
-                <img src="assets/images/icon.png" alt="<?= $app_details["name"] ?> Logo">
-                <h1 class="center-area__topbar__title"><?= $app_details["name"] ?></h1>
-                <p class="center-area__topbar__description"><?= $app_details["description"] ?></p>
-            </a>
-        </nav>
-        <div class="tabs tabs--show-cities">
-            <ul class="tabs__tabselect">
-                <li class="tabs__tabselect-button tabs__tabselect-button--cities">Cities</li>
-                <li class="tabs__tabselect-button tabs__tabselect-button--colors">Colors</li>
-                <li class="tabs__tabselect-button tabs__tabselect-button--about">About</li>
-            </ul>
-            <div class="tabs__sections">
-                <div class="tabs__sections-section tabs__sections-section--cities">
+    <div class="container">
+        <div class="center-area">
+            <nav class="center-area__topbar">
+                <a class="center-area__topbar-logo">
+                    <img src="assets/images/icon.png" alt="<?= $app_details["name"] ?> Logo">
+                    <h1 class="center-area__topbar-logo-title"><?= $app_details["name"] ?></h1>
+                    <p class="center-area__topbar-logo-description"><?= $app_details["description"] ?></p>
+                </a>
+            </nav>
+            <div class="tabs tabs--show-cities">
+                <ul class="tabs__tabselect">
+                    <li class="tabs__tabselect-button tabs__tabselect-button--cities">Cities</li>
+                    <li class="tabs__tabselect-button tabs__tabselect-button--colors">Colors</li>
+                    <li class="tabs__tabselect-button tabs__tabselect-button--about">About</li>
+                </ul>
+                <div class="tabs__sections">
+                    <div class="tabs__sections-section tabs__sections-section--cities">
 
-                </div>
-                <div class="tabs__sections-section tabs__sections-section--colors">
+                    </div>
+                    <div class="tabs__sections-section tabs__sections-section--colors">
 
-                </div>
-                <div class="tabs__sections-section tabs__sections-section--about">
-                    <?php
-                        $Parsedown = new Parsedown();
-                        echo $Parsedown->text(file_get_contents("assets/markdown/about.markdown"));
-                    ?>
+                    </div>
+                    <div class="tabs__sections-section tabs__sections-section--about">
+                        <?php
+                            $Parsedown = new Parsedown();
+                            echo $Parsedown->text(file_get_contents("assets/markdown/about.markdown"));
+                        ?>
+                    </div>
                 </div>
             </div>
         </div>
